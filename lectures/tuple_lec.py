9a47d2b26b56e294bb25f35630c395bb4cd46abd
Fahad Alsaigh <80637232+Fahad-salman@users.noreply.github.com>
2022-12-17 21:11:06 +0300

    tuple lecture

---
+++ lectures/tuple_lec.py
@@ -0,0 +1,8 @@
+# tuble what are tuble ?
+# like list but unlike list we can not modify them , tuple as a mutably list
+# tuple have two method it's count() and index() , we can use len()
+
+my_tuple = (1,2,3,4,5,6,7)
+print(my_tuple[3]) 
+# we can use it like any list , we can use sort , reverse whatever 
+# the idea is this value can not change .
