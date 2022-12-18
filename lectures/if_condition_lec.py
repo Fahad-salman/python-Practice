42f1c384da6ffdf74b568485520af15c664c0490
Fahad Alsaigh <80637232+Fahad-salman@users.noreply.github.com>
2022-12-18 11:07:53 +0300

    if condition lec

---
+++ lectures/if_condition_lec.py
@@ -0,0 +1,26 @@
+# if statement
+isOlder = True
+
+if isOlder: 
+    print('Yes is older')
+else:
+    print('no still young')
+
+# Ternary operator 
+
+# condition_if_True if condition else condition_if_False
+
+isFriend = True
+canMassage = "massage allowed " if isFriend else "massage not allowed"
+print(canMassage)
+
+# Short Circuiting 
+isFriend = True
+user = True
+if isFriend and user:
+    print('best friend forever')
+else:
+    print('not friend')
+
+# > , < , <= , >= , == , != 
+
