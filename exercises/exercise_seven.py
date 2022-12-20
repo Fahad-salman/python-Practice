04c10c022b49d52353871c6d2697aa305bd53043
Fahad Alsaigh <80637232+Fahad-salman@users.noreply.github.com>
2022-12-20 14:15:25 +0300

    Exercise seven

--- exercises/exercise_seven.py
+++ exercises/exercise_seven.py
@@ -7,6 +7,6 @@ def highest_even(get_list):
             if items not in list_evens: # To check there is no duplicate value
                 list_evens.append(items) # Add even numbers to list
     highest_even = max(list_evens) # Take max number == highest number
-    print(f'highest even nuber is {highest_even}') # Here we go this the answer 
+    print(f'highest even number is {highest_even}') # Here we go this the answer 
 
 highest_even([5,6,100,7,9,8,10,20,80])
