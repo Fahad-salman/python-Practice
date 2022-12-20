d3e9e19809095fed047c93606299fda58d00d870
Fahad Alsaigh <80637232+Fahad-salman@users.noreply.github.com>
2022-12-20 09:10:35 +0300

    Exercise 6

--- exercises/exercise_six.py
+++ exercises/exercise_six.py
@@ -5,13 +5,13 @@ words_list =[
 ]
 
 words_list.sort()
-doublecate = []
+doublecate_list = []
 
 for value in words_list: # here we just make a loop for list 
     if words_list.count(value) > 1: # then here we need a condition to check if doublecate or not
         # print (value)
-        if value not in doublecate: # after first check , check again if exsist in second list or not
-            doublecate.append(value) # add the doublecate value
-print(doublecate) 
+        if value not in doublecate_list: # after first check , check again if exsist in second list or not
+            doublecate_list.append(value) # add the doublecate value
+print(doublecate_list) 
 
 # Practice more and more for same like this case: !
