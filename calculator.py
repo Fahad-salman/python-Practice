744f85e5698e4e4420e8bd8187127f6f324bf8db
Fahad Alsaigh <80637232+Fahad-salman@users.noreply.github.com>
2022-12-27 14:32:09 +0300

    calculator

---
+++ calculator.py
@@ -0,0 +1,50 @@
+#  This function will colculate any number colculater
+
+def plus(one,tow):
+    return (one+tow)
+
+def minus(one,tow):
+    return (one-tow)
+
+def multiply(one,tow):
+    return (one*tow)
+
+def divide(one,tow):
+    return (one/tow)
+
+def culculate():
+    # name = input('Enter your name please : ')
+    # print('welcome ',name,' i like your n610ame')
+    while True:
+        try:
+            numberOne = float(input('enter the first number: '))
+        except ValueError:
+            print("Enter number only")
+            continue
+        else:
+            break
+    while True:
+        try:
+            numberTwo = float(input('enter the second number: '))
+        except ValueError:
+            print('enter number only')
+        else:
+            break
+    typeOperation = input('enter the type of operation: ')
+    if(typeOperation == '+' ):
+        print(numberOne, typeOperation ,numberTwo,' = ',plus(numberOne,numberTwo))
+    elif typeOperation == '-':
+        print(numberOne, typeOperation ,numberTwo,' = ',minus(numberOne,numberTwo))
+    elif typeOperation == '*' :
+        print(numberOne, typeOperation ,numberTwo,' = ', multiply(numberOne,numberTwo))
+    elif typeOperation == '/' :        
+        print(numberOne, typeOperation ,numberTwo,' = ',divide(numberOne,numberTwo))
+    else:
+        print("error: unknown type => try use one of them ( + , - , * , / )")
+
+while True:
+    res = input('stop or complete ? ')
+    if res == 'stop':
+        break
+    else:
+        culculate()
