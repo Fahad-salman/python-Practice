8777142aca39911d2d777124bdd98c78485f92bb
Fahad Alsaigh <80637232+Fahad-salman@users.noreply.github.com>
2022-12-29 12:16:28 +0300

    zip , filter ,reduce lecture

--- advanc/Advanced_one.py
+++ advanc/Advanced_one.py
@@ -1,13 +1,36 @@
-# map
-mapList = [1,2,3,4]
+from functools import reduce
+
+# functions
+
+def line():
+    print('=======================================')
+
 def mltiply_by2(number):
     return number*2
 
-print(list(map(mltiply_by2,mapList))) # will not change the other 
-print(mapList)
+def get_odd_number(number):
+    return number % 2 != 0
 
-# filter
+def accumulateor(acc,number):
+    print(acc,number)
+    return acc + number
 
-# zip
+# map function
+firstList = [1,2,3,4]
+line()
+print(list(map(get_odd_number,firstList)),'map()') # will not change the other 
+print(firstList)
 
-# reduce
+line()
+# filter function
+
+print(list(filter(get_odd_number,firstList)),'filter()') # will not change the other 
+line()
+# zip function
+
+otherList = [20,15,70]
+theirList = [80,30,90]
+print(list(zip(firstList,otherList,theirList)),'zip()') # will not change the other 
+line()
+# reduce function
+print(reduce(accumulateor,firstList,-100),'reduce()') # will not change the other 
