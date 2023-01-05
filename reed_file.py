d0bec6e9b94cf2ba184c8ed0624a95673265bb49
Fahad Alsaigh <80637232+Fahad-salman@users.noreply.github.com>
2023-01-05 14:47:04 +0300

    How to reed file in python

--- reed_file.py
+++ reed_file.py
@@ -4,3 +4,7 @@ print(my_file)
 print(my_file.read())
 print(my_file.readlines())
 print(my_file.readline())
+
+# after you done from file you have to close the file
+my_file.close()
+# just like this 
