8bc4c1d6fc5ec8d7e2f97152e3633054a0e192c8
Fahad Alsaigh <80637232+Fahad-salman@users.noreply.github.com>
2023-01-06 20:13:03 +0300

    How to reed file in python and how to write

--- reed_file.py
+++ reed_file.py
@@ -1,3 +1,4 @@
+# you can use path for specific ..\fileName\file.txt
 # How to reed file in python ?
 my_file = open('file_name.whatever')
 print(my_file)
@@ -7,4 +8,9 @@ print(my_file.readline())
 
 # after you done from file you have to close the file
 my_file.close()
-# just like this 
+# just like this or you can use
+with open('file_name.whatever') as my_file:
+    print(my_file.read())
+# also you can write 
+with open('file_name.whatever',mode='a') as my_file:
+    text = my_file.write('Hello world! ')
