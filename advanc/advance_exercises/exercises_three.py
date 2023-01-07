6155bbf0df61f32a9d2ade6e38142e1ade50d6e4
Fahad Alsaigh <80637232+Fahad-salman@users.noreply.github.com>
2023-01-07 20:28:53 +0300

    Exercises 3

--- advanc/advance_exercises/exercises_three.py
+++ advanc/advance_exercises/exercises_three.py
@@ -2,7 +2,7 @@
 import re
 
 pattern = re.compile(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)")
-user_email = 'Fahad-Salman@gcom'
+user_email = input('Enter your email address : ')
 
 res = pattern.search(user_email)
 # print(res)
@@ -11,3 +11,16 @@ if res == None:
     print('Please enter your email address correctly')
 else:
     print(res)
+
+# password checker
+
+pat = re.compile(r'[A-Za-z0-9$%#@!?]{8,}')
+password = input('Enter your password : ')
+chek = pat.fullmatch(password)
+
+if chek == None:
+    print('password invalid')
+else:
+    print(chek)  
+    
+    
