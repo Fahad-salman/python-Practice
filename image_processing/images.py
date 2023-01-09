381101490ba26b7d0593ae3958cf3cfaae80329f
Fahad Alsaigh <80637232+Fahad-salman@users.noreply.github.com>
2023-01-09 12:44:32 +0300

    Image processing lecture

---
+++ image_processing/images.py
@@ -0,0 +1,40 @@
+from PIL import Image , ImageFilter
+
+# How to filter any image you want 
+# First of all chose any image you want to filter
+
+# Here i disided to filter this image , open the image just like this
+reddington_image = Image.open('./images/reddington_image.jpg')
+ragnar_lothbrok_image = Image.open('./images/ragnar-lothbrok_image.jpg')
+
+# then we can do alot of things like filter or rotate or resize or else
+# Here we will filter our image 
+filtered_image = reddington_image.filter(ImageFilter.SMOOTH)
+
+# Then we will save the changes 
+# first prameter name for your image then put the path for the new image and use (png)
+# filtered_image.save("AnyNameYouWant.png",'png')
+# if you run your code , you will save your image with changes
+
+# You can open new images with the change directly without saving the image
+# very simple just write 
+filtered_image.show()
+
+# How to rotate our image ?
+crooked = filtered_image.rotate(180) 
+# then open the image 
+crooked.show()
+
+# How to resize your image ?
+resize = filtered_image.resize((300,300))
+resize.show()
+
+# You can change the color of your image by doing this
+convert_img = reddington_image.convert('L')
+convert_img.show()
+
+# You can crop your image 
+box = (100,300,950,800)
+crop_image = ragnar_lothbrok_image.crop(box)
+crop_image.show()
+
