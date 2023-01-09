161eda561437aa1e4e0766022e6be3320bf9f02e
Fahad Alsaigh <80637232+Fahad-salman@users.noreply.github.com>
2023-01-09 13:05:41 +0300

    Image processing lecture

--- image_processing/images.py
+++ image_processing/images.py
@@ -9,7 +9,7 @@ ragnar_lothbrok_image = Image.open('./images/ragnar-lothbrok_image.jpg')
 
 # then we can do alot of things like filter or rotate or resize or else
 # Here we will filter our image 
-filtered_image = reddington_image.filter(ImageFilter.SMOOTH)
+# filtered_image = reddington_image.filter(ImageFilter.SMOOTH)
 
 # Then we will save the changes 
 # first prameter name for your image then put the path for the new image and use (png)
@@ -18,23 +18,38 @@ filtered_image = reddington_image.filter(ImageFilter.SMOOTH)
 
 # You can open new images with the change directly without saving the image
 # very simple just write 
-filtered_image.show()
+# filtered_image.show()
 
 # How to rotate our image ?
-crooked = filtered_image.rotate(180) 
+# crooked = filtered_image.rotate(180) 
 # then open the image 
-crooked.show()
+# crooked.show()
 
 # How to resize your image ?
-resize = filtered_image.resize((300,300))
-resize.show()
+# resize = filtered_image.resize((300,300))
+# resize.show()
 
 # You can change the color of your image by doing this
-convert_img = reddington_image.convert('L')
-convert_img.show()
+# convert_img = reddington_image.convert('L')
+# convert_img.show()
 
 # You can crop your image 
-box = (100,300,950,800)
-crop_image = ragnar_lothbrok_image.crop(box)
-crop_image.show()
+# box = (100,300,950,800)
+# crop_image = ragnar_lothbrok_image.crop(box)
+# crop_image.show()
 
+# thumbnail very very useful 
+print(ragnar_lothbrok_image.size)
+ragnar_lothbrok_image.thumbnail((500,500))
+ragnar_lothbrok_image.save('thumbnail.png','png')
+
+# resize_img = ragnar_lothbrok_image.resize((650,400))
+# resize_img.save('ragnar.png','png')
+# resize_img.show()
+
+# new_img = Image.open('ragnar.png')
+# new_img.thumbnail()
+# new_img.show()
+# print(new_img.size)
+
+# if you want to try any of this code line gust take of the commente
