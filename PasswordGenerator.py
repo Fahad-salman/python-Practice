6dc90d0715d96a2227335480a364f5efb8165341
Fahad Alsaigh <80637232+Fahad-salman@users.noreply.github.com>
2023-01-22 09:33:08 +0300

    Password Generator

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -1,11 +1,14 @@
 import random
 
-chars = ['a', 'b', 'c', 'd', 'e', 'f', 'g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','0','1','2','3','4','5','6','7','8','9','!','@','#','$']
+chars = ['a', 'b', 'c', 'd', 'e', 'f', 'g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','0','1','2','3','4','5','6','7','8','9','!','@','#','$','?']
 
 def generate_password():
     while True:
         try:
             pwd_longer = int(input('How long do you want your password to be? '))
+            if pwd_longer == 0:
+                print('Password length must be greater than 0.')
+                continue
         except ValueError :
             print('Please enter a number.')
         else:
