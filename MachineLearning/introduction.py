7e0ddc8f4afaf694da10441da4529c54dff75c58
Fahad Alsaigh <80637232+Fahad-salman@users.noreply.github.com>
2023-02-02 10:24:46 +0300

    Machine Learning introduction

---
+++ MachineLearning/introduction.py
@@ -0,0 +1,7 @@
+#1 First thing will import the data from our csv file or database.
+#2 Second thing will clean the data - to have perfect data of the columns and rows , to get the good result.
+#3 We will split the data , training set and test set .
+#4 Createing a Model, there is a package or library called sklearn.
+#5 Check the output.
+#6 Improv
+
--- automation.py
+++ automation.py
@@ -4,21 +4,48 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 import pprint
-import time as m
+import time as nap
 
 options = webdriver.ChromeOptions()
 options.add_experimental_option('detach',True)
 chrome_browser = webdriver.Chrome(options=options,service=Service(ChromeDriverManager().install()))
 
-chrome_browser.maximize_window()
-chrome_browser.get("") # put your url here
+# chrome_browser.maximize_window()
+# chrome_browser.get("") # put your url here
 
-get_to = chrome_browser.find_element(By.ID,"") # you can use id or name or xpath or class_name
-get_to.click() 
-m.sleep(1)
-get_to.send_keys('') # this is the text you want to enter
+# get_to = chrome_browser.find_element(By.ID,"") # you can use id or name or xpath or class_name
+# get_to.click() 
+# m.sleep(1)
+# get_to.send_keys('') # this is the text you want to enter
 
-chrome_browser.close() # when you are done, close the browser
+# chrome_browser.close() # when you are done, close the browser
 
 # pip install selenium
 # https://selenium-python.readthedocs.io/installation.html
+
+# list for id that will be used in function
+list_of_ids = ['']
+list_of_inputs = ['']
+
+def automation():
+    chrome_browser.maximize_window()
+    chrome_browser.get("")
+    for index in list_of_ids:
+        get_to = chrome_browser.find_element(By.ID,index)
+        nap.sleep(1)
+        if index == 0:
+            get_to.click()
+            nap.sleep(1)
+        else:
+            get_to.click()
+            nap.sleep(1)
+            for massage in list_of_inputs:
+                get_to.send_keys(massage)
+                nap.sleep(1)
+                
+        break
+
+automation()
+
+
+#  in progress ....
