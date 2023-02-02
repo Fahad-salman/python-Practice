6d6ff01c15bcb678099478f7d47500c10e3f073d
Fahad Alsaigh <80637232+Fahad-salman@users.noreply.github.com>
2023-02-02 11:16:31 +0300

    Machine Learning introduction

--- MachineLearning/introduction.py
+++ MachineLearning/introduction.py
@@ -1,7 +1,15 @@
 #1 First thing will import the data from our csv file or database.
 #2 Second thing will clean the data - to have perfect data of the columns and rows , to get the good result.
 #3 We will split the data , training set and test set .
-#4 Createing a Model, there is a package or library called sklearn.
+#4 Createing a Model, there is a package or library called sklearn and NumPy and Pandas.
 #5 Check the output.
 #6 Improv
 
+# - NumPy -> to help us to use list and arrays in python in spesificly multidimensional arrays . 
+# - Pandas -> to use data anlysis , to give us an idea of this data frame allow functionality to manipulate data . for csv file or tabular data.
+# - Scikit-learn -> to help us to use machine learning algorithms.
+# - matplotlib -> charting data 
+
+# jupyter notebook it's important for machine learning, to step through the data to understand the data
+
+#  last thing we need data to work with , thers is a tool called kaggle , there is free data to work with.
