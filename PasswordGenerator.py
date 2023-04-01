4b39b8a662ae052d9a7b0d73d8b47ef5da72eb70
Fahad Alsaigh <80637232+Fahad-salman@users.noreply.github.com>
2023-04-01 17:22:25 +0300

    generate_password_v2

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -1,4 +1,5 @@
 import random
+import string
 
 chars = ['a', 'b', 'c', 'd', 'e', 'f', 'g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','0','1','2','3','4','5','6','7','8','9','!','@','#','$','?']
 
@@ -17,3 +18,15 @@ def generate_password():
     print('Here is your password:',''.join(pwd_list))
     
 generate_password()
+
+# ---------------------------------------------------------------- 
+
+def generate_password_v2(lng):
+    """Generates a random password with the given length"""
+    letters = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(random.choice(letters) for i in range(lng))
+    return password
+
+psd = int(input('Enter leangt password : '))
+password = generate_password_v2(psd)
+print("Generated password:", password)
