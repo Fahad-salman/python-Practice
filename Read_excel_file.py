9fc08634d1b1fbeba7bf52cec1f5d44c96963536
Fahad Alsaigh <80637232+Fahad-salman@users.noreply.github.com>
2024-10-28 15:16:01 +0300

    Chose the data type for specific column

--- Read_excel_file.py
+++ Read_excel_file.py
@@ -4,5 +4,9 @@ import pandas
 
 excel_data_df = pandas.read_excel('file_name.xlsx', sheet_name='Sheet1')
 
+# In this line you chose the data type for specific column .
+excel_data_df_2 = pandas.read_excel('file_name.xlsx', sheet_name='Sheet1',dtype={"age":int,"salary":float})
+
+
 # print whole sheet data
 print(excel_data_df)
